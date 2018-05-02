620b32dadea6624dc8d774cf6d8d3427591d65b8
kazurayam <kazuaki.urayama@gmail.com>
2018-05-03 07:16:36 +0900

    refactored Test Object 'li_- Please Select -'

--- Object Repository/Page_14771/li_- Please Select -.rs
+++ Object Repository/Page_14771/li_- Please Select -.rs
@@ -7,30 +7,30 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//li[@id = 'e12345f3-02b2-1111-1faf-bcf11a22bef0' and (text() = '- Please Select -' or . = '- Please Select -')]</value>
+         <value>//ul[@id='ManufacturerList_listbox']/li[text() = '- Please Select -']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
       <value>li</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
       <value>- Please Select -</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//ul[@id='ManufacturerList_listbox']/li[0]</value>
+      <value>//ul[@id='ManufacturerList_listbox']/li[text() = '- Please Select -']</value>
    </webElementProperties>
 </WebElementEntity>
